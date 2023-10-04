c01efdf35cac8aadd785400bb76bf921130640f6
Syifa Iswardy <syifaiswardy@gmail.com>
2023-10-04 15:10:36 +0800

    refactor ui

--- resources/views/adminpage.blade.php
+++ resources/views/adminpage.blade.php
@@ -32,7 +32,7 @@
               <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
                   <h3 class="font-weight-bold" style="font-size:150%;">Welcome {{$username}}</h3>
-                  <h6 class="font-weight-normal mb-0">View your daily dashboard here!</h6>
+                  <h6 class="font-weight-normal mb-0">View your dashboard here!</h6>
                 </div>
                
               </div>
